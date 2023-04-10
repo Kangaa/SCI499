2a2aa35f38405423f12744c2311571fb280792dc
Kangaa <j.reubender@gmail.com>
2023-04-10 13:10:02 +1000

    Package refactoring now main 'SpatialSEIR' package

--- src/SCI499.jl
+++ src/SCI499.jl
@@ -1,10 +1,12 @@
 
 module SCI499
+
+
 using DrWatson
 
 include(srcdir("MixingMatrices_src.jl"))
 include(srcdir("JRB_Spatial_SEIR_src.jl"))
 
 using Reexport
-@reexport using .SpatialSEIR, .MixingMatrices, Shapefile, Tables, DataFrames
+@reexport using .SpatialSEIR, .MixingMatrices, Shapefile, Tables, DataFrames, CSV
 end
--- src/SpatialSEIR_src.jl
+++ src/SpatialSEIR_src.jl
@@ -1,19 +1,130 @@
 """
 This is the set of types and methods necessary to run a spatial SEIR Model equivalent to that in Moss et al. 2018
 """
-module SpatialSEIR
-    using DataFrames
-    export MixingMatrix
-    export Parameters
-    export new_model
-    export run_sim
-    
+
+
+module MixingMatrices
+
+using Shapefile
+using Tables
+using DataFrames
+
+export SpatialMixingMatrix
+
+
 struct MixingMatrix
     mm::Array{Real, 2}
     names::Vector{String}
     popns::Array{Int, 1}
 end
 
+
+function SpatialMixingMatrix(Codes, intra = 0.9)
+    
+    npatch = nrow(Codes)
+    
+    MM = fill(1.0, npatch, npatch)
+    
+    intraregioncoefficient = intra
+    interregioncoefficient = 1.0-intraregioncoefficient
+    
+    for i in eachindex(Codes[:,1])
+        for j in eachindex(Codes[:, 1])
+            for l in 1:(ncol(Codes)-1)
+                if Codes[i,l] == Codes[j,l]
+                    nLLinUL = (Codes[i,l] .== Codes[:, l])|> sum
+                    coef = (intraregioncoefficient/nLLinUL)
+                    MM[i,j] *= coef
+                    break
+                else
+                    if l == (ncol(Codes)-1) 
+                        nLLoutUL = (Codes[i,l] .!= Codes[:, l])|> sum
+                        MM[i,j] *= interregioncoefficient/nLLoutUL
+                    else
+                        coef = interregioncoefficient
+                        MM[i,j] *= coef
+                    end
+                end
+            end
+        end
+    end
+    
+    return MM
+end
+
+function  getCodes(shapetable)
+    shapefields = shapetable |>
+    Tables.columnnames .|>
+    String 
+    lowerCodes = Tables.getcolumn(shapetable, findfirst(endswith.(shapefields, "CODE21")))
+
+    Code_DF = DataFrame(ll = lowerCodes)
+
+    rename!(Code_DF, 1 => MixingMatrices.getSARegion(Code_DF[1,1]))
+    
+    while MixingMatrices.getSARegion(lowerCodes[1]) != "S/T"
+        upperCodes = DataFrame(new = MixingMatrices.getNextLevelUpCode.(lowerCodes))
+        rename!(upperCodes, 1 => MixingMatrices.getSARegion(upperCodes[1,1]))
+        Code_DF = hcat(Code_DF, upperCodes)
+        lowerCodes = upperCodes[:,1]
+    end
+    Code_DF
+end
+
+
+function  getCodes(shapetable::DataFrame)
+    shapefields = shapetable |>
+    Tables.columnnames .|>
+    String 
+    lowerCodes = Tables.getcolumn(shapetable, findfirst(endswith.(shapefields, "CODE21")))
+
+    Code_DF = DataFrame(ll = lowerCodes)
+
+    rename!(Code_DF, 1 => MixingMatrices.getSARegion(Code_DF[1,1]))
+    
+    while MixingMatrices.getSARegion(lowerCodes[1]) != "S/T"
+        upperCodes = DataFrame(new = MixingMatrices.getNextLevelUpCode.(lowerCodes))
+        rename!(upperCodes, 1 => MixingMatrices.getSARegion(upperCodes[1,1]))
+        Code_DF = hcat(Code_DF, upperCodes)
+        lowerCodes = upperCodes[:,1]
+    end
+    Code_DF
+end
+
+function getSARegion(SAcode)
+    x = SAcode|>length
+    x == 1 ? "S/T" :
+    x == 3 ? "SA4" :
+    x == 5 ? "SA3" :
+    x == 9 ? "SA2" :
+    x == 11 ? "SA1" :
+    "Invalid Code"
+end
+
+function getfirstn(string::String, n::Int64)
+    string[1:n]
+end
+
+function getNextLevelUpCode(Code)
+    SA_level = getSARegion(Code)
+    if SA_level == "S/T"
+        throw(DomainError(SA_level, "State/Territory level (no higher level)"))
+    elseif SA_level== "SA2"
+        upcode = Code[1:(length(Code)-4)]
+    else
+        upcode = Code[1:(length(Code)-2)]
+    end
+
+    return upcode
+end
+end
+module SpatialSEIR
+include(srcdir)
+    using DataFrames
+    export Parameters
+    export new_model
+    export run_sim
+    
 struct Parameters
     β::Float64
     σ::Float64
