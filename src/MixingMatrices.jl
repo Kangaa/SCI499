68878c488b751c4327b2862ad8033c9d691bf36e
Kanga <j.reubender@gmail.com>
2023-07-21 23:56:47 +1000

    final mm def

--- src/MixingMatrices.jl
+++ src/MixingMatrices.jl
@@ -11,29 +11,33 @@ function SpatialMixingMatrix(llCodepop, ξ, μ = 0.5)
     
     Popns = Vector{Dict{String, Int}}()
 
-    for i in 1:ncol(Codes)-1
-        Popns[i] = Codes |> x -> 
-            leftjoin(x, llCodepop, on = (:SA2 => Symbol("SA2 code"))) |> x ->
+    for i in 1:ncol(Codes)
+        Codes |> x -> 
+            leftjoin(x, llCodepop, on = (Symbol(names(Codes)[1]) => Symbol(names(llCodepop)[1]))) |> x ->
             groupby(x, i) |> x ->
-            combine(x, :Pop => sum => :Pop) |> x ->
-            Pair.(x[:,i], x.Pop) 
+            combine(x, Symbol(names(llCodepop)[2]) => sum => :Pop) |> x ->
+            Pair.(x[:,1], x.Pop) |> x ->
+            Dict(x) |> x ->
+            push!(Popns, x)
     end
     
     SAMM = [SAMMij(i, j, ξ, Codes, Popns) for i in eachindex(Codes[:,1]), j in eachindex(Codes[:,1])]
 
-    PPMM = [PPMMij(i, j, Popns) for i in eachindex(Codes[:,1]), j in eachindex(Codes[:,1])]
+    PPMM = [PPMMij(i, j, llCodepop[:,2]) for i in eachindex(Codes[:,1]), j in eachindex(Codes[:,1])]
 
-    MMM = ((μ*MM) + ((1-μ)*PPMM))
+    MMM = ((μ*SAMM) + ((1-μ)*PPMM))
 
     return MMM
 end
 
-function SAMMij(i, j, ξ, codes, popns)
+function SAMMij(i, j, ξ, Codes, Popns)
     for l in eachindex(ξ)
-        if codes[i, l] == codes[j, l]
-            l == 1 && return ξ[l] 
-            ll_pop = popns[1]|> x -> get(x, codes[j, 1], false) 
-            ul_pop = get(popns[l], codes[i, l], false) - get(popns[l-1], codes[i, l-1], false)
+        if Codes[i, l] == Codes[j, l]
+
+            l == 1 && return ξ[1]
+
+            ll_pop = Popns[1]|> x -> get(x, Codes[j, 1], false) 
+            ul_pop = get(Popns[l], Codes[i, l], false) - get(Popns[l-1], Codes[i, l-1], false)
             return ξ[l] * (ll_pop/ul_pop)
         end
     end
@@ -41,24 +45,6 @@ end
 
 PPMMij(i, j, popns) = popns[j]/sum(popns)
 
-function  expandCodes(shapetable::Shapefile.Table)
-    shapefields = shapetable |>
-    Tables.columnnames .|>
-    String 
-    lowerCodes = Tables.getcolumn(shapetable, findfirst(endswith.(shapefields, "CODE21")))
-
-    Code_DF = DataFrame(ll = lowerCodes)
-
-    rename!(Code_DF, 1 => getSARegion(Code_DF[1,1]))
-    
-    while getSARegion(lowerCodes[1]) != "S/T"
-        upperCodes = DataFrame(new = getNextLevelUpCode.(lowerCodes))
-        rename!(upperCodes, 1 => getSARegion(upperCodes[1,1]))
-        Code_DF = hcat(Code_DF, upperCodes)
-        lowerCodes = upperCodes[:,1]
-    end
-    Code_DF
-end
 
 function  expandCodes(codes::Vector)
     lowerCodes = string.(codes)
